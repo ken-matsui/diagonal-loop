c0f65010a814a922f4e4a0421ab7066a3e6f4a6b
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-03-04 04:49:54 -0800

    Implement ave_diagonal_len for dim2

--- src/dim2.rs
+++ src/dim2.rs
@@ -14,6 +14,7 @@ pub(crate) fn diag_loop(X: usize, Y: usize) {
 
     println!("diagonals: {}", count_diagonals(X, Y));
     println!("max diagonal len (middle): {}", max_diagonal_len(X, Y));
+    println!("ave diagonal len: {:.2}", ave_diagonal_len(X, Y));
 
     for offset in 0..=(X + Y - 2) {
         for y in 0..=offset {
@@ -26,11 +27,17 @@ pub(crate) fn diag_loop(X: usize, Y: usize) {
     }
 }
 
+#[inline]
 fn count_diagonals(x: usize, y: usize) -> usize {
     (x + y) - 1
 }
+#[inline]
 fn max_diagonal_len(x: usize, y: usize) -> usize {
     std::cmp::min(x, y)
+}
+#[inline]
+fn ave_diagonal_len(x: usize, y: usize) -> f32 {
+    (x * y) as f32 / count_diagonals(x, y) as f32
 }
 
 #[cfg(test)]
@@ -87,5 +94,31 @@ mod tests {
         assert_eq!(max_diagonal_len(1, 3), real_max_diagonal_len(1, 3));
         assert_eq!(max_diagonal_len(2, 100), real_max_diagonal_len(2, 100));
         assert_eq!(max_diagonal_len(100, 200), real_max_diagonal_len(100, 200));
+    }
+
+    #[allow(non_snake_case)]
+    fn real_ave_diagonal_len(X: usize, Y: usize) -> f32 {
+        let mut element_count = 0.;
+        let mut diag_count = 0.;
+        for offset in 0..=(X + Y - 2) {
+            for y in 0..=offset {
+                let x = offset - y;
+                if x < X && y < Y {
+                    element_count += 1.;
+                }
+            }
+            diag_count += 1.;
+        }
+        element_count / diag_count
+    }
+
+    #[test]
+    fn test_ave_diagonal_len() {
+        assert_eq!(ave_diagonal_len(1, 1), real_ave_diagonal_len(1, 1));
+        assert_eq!(ave_diagonal_len(2, 2), real_ave_diagonal_len(2, 2));
+        assert_eq!(ave_diagonal_len(1, 2), real_ave_diagonal_len(1, 2));
+        assert_eq!(ave_diagonal_len(1, 3), real_ave_diagonal_len(1, 3));
+        assert_eq!(ave_diagonal_len(2, 100), real_ave_diagonal_len(2, 100));
+        assert_eq!(ave_diagonal_len(100, 200), real_ave_diagonal_len(100, 200));
     }
 }
